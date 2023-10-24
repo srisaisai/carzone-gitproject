46524ac26b428553bf4d075e134d97c6c437c2b4
srisaisai <saihemadudes@gmail.com>
2023-10-24 07:43:18 +0530

    2 nd commit

--- pages/views.py
+++ pages/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.shortcuts import HttpResponse
 # Create your views here.
 def home(request):
-    return HttpResponse('Hello world')
+    return render (request , 'pages/index.html')
