83f7675a7892b557614baa602a4fd6050c758441
srisaisai <saihemadudes@gmail.com>
2023-10-24 16:35:13 +0530

    complted teams model

--- carzone/settings.py
+++ carzone/settings.py
@@ -125,3 +125,5 @@ STATIC_URL = '/static/'
 STATIC_ROOT = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),]
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
+from .models import Team
 # Create your views here.
+
+
 def home(request):
-    return render (request , 'pages/index.html')
+    teams = Team.objects.all()
+    context = {
+        'teams': teams
+    }
+    return render(request, 'pages/index.html', context)
+
+
+def about(request):
+    return render(request, 'pages/about.html', {})
+
+
+def services(request):
+    return render(request, 'pages/services.html', {})
+
+
+def contact(request):
+    return render(request, 'pages/contact.html', {})
